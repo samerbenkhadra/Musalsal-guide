c60e34ba23898e82d8fd2c5b6789279d93d2b5c1
Samer <samerbenka@hotmail.com>
2026-05-12 15:44:49 +0100

    Add config plugin to embed ADI registration token in Android assets

---
+++ plugins/withAdiRegistration.js
@@ -0,0 +1,22 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const withAdiRegistration = (config) => {
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const assetsDir = path.join(config.modRequest.platformProjectRoot, 'app', 'src', 'main', 'assets');
+      if (!fs.existsSync(assetsDir)) {
+        fs.mkdirSync(assetsDir, { recursive: true });
+      }
+      fs.writeFileSync(
+        path.join(assetsDir, 'adi-registration.properties'),
+        'CLX4OJ3LEDHO2AAAAAAAAAAAAA'
+      );
+      return config;
+    },
+  ]);
+};
+
+module.exports = withAdiRegistration;
